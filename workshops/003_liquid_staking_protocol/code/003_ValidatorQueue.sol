63a7c99fe9376f8f762c3f423924ef06a7310ab3
Dhruvin Parikh <29928916+dhruvinparikh@users.noreply.github.com>
2024-04-25 18:12:32 -0400

    Update 003_ValidatorQueue.sol

--- workshops/003_liquid_staking_protocol/code/003_ValidatorQueue.sol
+++ workshops/003_liquid_staking_protocol/code/003_ValidatorQueue.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.13;
 
-
 struct Validator {
     bytes pubKey;
     bytes signature;
@@ -11,7 +10,7 @@ struct Validator {
 }
 
 struct ValidatorDeque {
-    int128 _begin; 
+    int128 _begin;
     int128 _end;
     mapping(int128 => Validator) _validators;
 }
@@ -21,139 +20,127 @@ error EmptyQueue();
 error InvalidIndexRanges();
 
 library ValidatorQueue {
+    function add(
+        ValidatorDeque storage deque,
+        Validator memory _validator,
+        bytes memory withdrawCredentials
+    ) internal {
+        int128 backIndex = deque._end;
+        deque._validators[backIndex] = _validator;
+        deque._end = backIndex + 1;
+    }
 
-        function add(
-            ValidatorDeque storage deque, 
-            Validator memory _validator,
-            bytes memory withdrawCredentials
-        ) internal {
-            int128 backIndex = deque._end;
-            deque._validators[backIndex] = _validator;
-            deque._end =  backIndex + 1;
+    // Order or Unordered
+    function removeOrdered(ValidatorDeque storage deque, uint256 removeIndex)
+        internal
+        returns (bytes memory removedPubKey)
+    {
+        int128 idx = deque._begin + removeIndex;
+
+        if (idx >= deque._end) revert OutOfBounds();
+
+        removedPubKey = deque._validators[idx].pubKey;
+
+        for (int128 i = idx; i < deque._end; i++) {
+            deque._validators[i] = deque._validators[i + 1];
         }
 
-        // Order or Unordered
-        function removeOrdered(
-            ValidatorDeque storage deque,
-            uint256 removeIndex
-        ) internal returns (bytes memory removedPubKey){
-            int128 idx = deque._begin + removeIndex;
+        pop(deque, 1);
+    }
 
-            if (idx >= deque._end) revert OutOfBounds();
+    function removeUnordered(ValidatorDeque storage deque, uint256 removeIndex)
+        internal
+        returns (bytes memory removedPubKey)
+    {
+        int128 idx = deque._begin + removeIndex;
 
-            removedPubKey = deque._validators[idx].pubKey;
+        if (idx >= deque._end) revert OutOfBounds();
 
-            for (int128 i = idx; i < deque._end ; i++){
-                deque._validators[i] = deque._validators[i+1];
-            }
+        removedPubKey = deque._validators[idx].pubKey;
 
-            pop(deque,1);
+        uint256 lastIndex = count(deque) - 1;
 
+        // swap
+        if (removeIndex != lastIndex) {
+            swap(deque, removeIndex, lastIndex);
         }
 
-        function removeUnordered(
-            ValidatorDeque storage deque,
-            uint256 removeIndex
-        ) internal returns (bytes memory removedPubKey){
-            int128 idx = deque._begin + removeIndex;
+        // pop
+        pop(deque, 1);
+    }
 
-            if (idx >= deque._end) revert OutOfBounds();
+    function pop(ValidatorDeque storage deque, uint256 times)
+        internal
+        returns (Validator memory validator)
+    {
+        for (uint256 i; i < times; i++) {
+            if (isEmpty(deque)) EmptyQueue();
 
-            removedPubKey = deque._validators[idx].pubKey;
+            int128 backIndex = deque._end - 1;
 
-            uint256 lastIndex = count(deque) - 1;
-            
-            // swap
-            if (removeIndex != lastIndex) {
-            swap(deque,removeIndex,lastIndex);
-            }
-
-            // pop
-            pop(deque,1);
-
+            validator = deque._validators[backIndex];
+            delete deque._validators[backIndex];
+            deque._end = backIndex;
         }
+    }
 
-        function pop(
-            ValidatorDeque storage deque,
-            uint256 times
-        ) internal returns (Validator memory validator) {
-            for (uint256 i; i < times ; i++) {
-                if (isEmpty(deque)) EmptyQueue();
+    function swap(
+        ValidatorDeque storage deque,
+        uint256 fromIndex,
+        uint256 toIndex
+    ) internal {
+        if (fromIndex == toIndex) revert InvalidIndexRanges();
 
-                int128 backIndex = deque._end - 1;
+        if (isEmpty(deque)) revert EmptyQueue();
 
-                validator = deque._validators[backIndex];
-                delete deque._validators[backIndex];
-                deque._end = backIndex;
-            }
-        }
+        int128 fromIdx = deque._begin + fromIndex;
+        if (fromIdx >= deque._end) revert OutOfBounds();
 
-        function swap(
-            ValidatorDeque storage deque,
-            uint256 fromIndex,
-            uint256 toIndex
-        ) internal {    
+        int128 toIdx = deque._begin + toIndex;
+        if (toIdx >= deque._end) revert OutOfBounds();
 
-            if (fromIndex == toIndex) revert InvalidIndexRanges();
+        Validator memory _fromVal = deque._validators[fromIdx];
+        Validator memory _toVal = deque._validators[toIdx];
 
-            if(isEmpty(deque)) revert EmptyQueue();
+        deque._validators[fromIdx] = _toVal;
+        deque._validators[toIdx] = _fromVal;
+    }
 
-            int128 fromIdx = deque._begin + fromIndex;
-            if (fromIdx >= deque._end) revert OutOfBounds();
+    function isEmpty(ValidatorDeque storage deque) internal returns (bool) {
+        return deque._end <= deque._begin;
+    }
 
-            int128 toIdx = deque._begin + toIndex;
-            if (toIdx >= deque._end) revert OutOfBounds();
+    function getNext(ValidatorDeque storage deque)
+        internal
+        returns (Validator memory popedValidator)
+    {
+        if (isEmpty(deque)) revert EmptyQueue();
 
-            Validator memory _fromVal = deque._validators[fromIdx];
-            Validator memory _toVal = deque._validators[toIdx];
+        int128 frontIndex = deque._begin;
+        popedValidator = deque._validators[frontIndex];
 
-            deque._validators[fromIdx] = _toVal;
-            deque._validators[toIdx] = _fromVal;
-        }
+        delete deque._validators[frontIndex];
 
-        function isEmpty(
-            ValidatorDeque storage deque
-        ) internal returns (bool){
-            return deque._end <= deque._begin;
-        }
+        deque._begin = frontIndex + 1;
+    }
 
-        function getNext(
-            ValidatorDeque storage deque
-        ) internal returns (Validator memory popedValidator){
-            if (isEmpty(deque)) revert EmptyQueue();
+    function get(ValidatorDeque storage deque, uint256 index)
+        internal
+        returns (Validator memory validator)
+    {
+        int128 idx = deque._begin + index;
 
-            int128 frontIndex = deque._begin;
-            popedValidator = deque._validators[frontIndex];
+        if (idx >= deque._end) revert OutOfBounds();
 
-            delete deque._validators[frontIndex];
+        validator = deque._validators[idx];
+    }
 
-            deque._begin = frontIndex + 1;
+    function clear(ValidatorDeque storage deque) internal {
+        deque._end = 0;
+        deque._begin = 0;
+    }
 
-
-        }
-
-        function get(
-            ValidatorDeque storage deque,
-            uint256 index
-        ) internal returns (Validator memory validator){
-            int128 idx = deque._begin + index;
-
-            if (idx >= deque._end) revert OutOfBounds();
-
-            validator = deque._validators[idx];
-        }
-
-        function clear(
-            ValidatorDeque storage deque
-        ) internal {
-            deque._end = 0;
-            deque._begin = 0;
-        }
-
-        function count(
-            ValidatorDeque storage deque
-        ) internal returns (uint256){
-            deque._end - deque._begin;
-        }
-
+    function count(ValidatorDeque storage deque) internal returns (uint256) {
+        deque._end - deque._begin;
+    }
 }
